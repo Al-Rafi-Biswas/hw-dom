a8f64e710d069be3b5095636bbcfb17fcb5330e4
Al-Rafi-Biswas <almd88560@gmail.com>
2024-10-29 18:27:38 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,197 @@
+// first background change......
+
+let back = document.getElementById("background");
+back.style.backgroundColor = "wheat";
+back.style.height = "auto";
+back.style.padding = "10px";
+
+// h1 tag desinge............
+
+let head = document.getElementsByTagName("h1")[0];
+head.style.textAlign = "center";
+head.style.backgroundColor = "red";
+head.style.color = "white";
+head.style.padding = "5px";
+
+// div1 strat..................................................
+
+let div1Parent = document.getElementById("parent1");
+div1Parent.style.display = "Flex";
+div1Parent.style.justifyContent = "center";
+div1Parent.style.justifyItems = "center";
+div1Parent.style.gap = "40px";
+console.log(div1Parent);
+
+// first child...............
+
+let divChild1 = document.getElementsByClassName("child")[0];
+divChild1.style.backgroundColor = "red";
+divChild1.style.height = "200px";
+divChild1.style.width = "300px";
+divChild1.style.border = "solid";
+divChild1.style.border = "0px";
+divChild1.style.borderRadius = "5px";
+console.log(divChild1);
+
+//first child div er vitore interhtml er use...........
+
+// let underList = document.getElementById("ul");
+
+// let ulHeading = document.getElementById("ulhead");
+// ulHeading.style.fontSize = "20px";
+// ulHeading.style.color = "white";
+// ulHeading.style.textAlign = "center";
+// console.log(ulHeading);
+
+// underList.innerHTML =
+//   "<li> first list </li>  <li> first list </li> <li> first list </li> <li> first list </li> ";
+// underList.style.color = "green";
+// underList.style.fontSize = "18px";
+// console.log(underList);
+
+let ulHeading1 = document.getElementById("ulhead1");
+ulHeading1.style.fontSize = "20px";
+ulHeading1.style.color = "white";
+ulHeading1.style.textAlign = "center";
+console.log(ulHeading1);
+
+let underList1 = document.getElementById("ul1");
+underList1.innerHTML =
+  "<li> second list </li> <li> second list </li> <li> second list </li> <li> second list </li> <li> second list </li>";
+underList1.style.fontSize = "18px";
+underList1.style.color = "black";
+console.log(underList1);
+
+//second child...........
+
+let divChild2 = document.getElementsByClassName("child")[1];
+divChild2.style.backgroundColor = "red";
+divChild2.style.height = "200px";
+divChild2.style.width = "300px";
+divChild2.style.border = "2px";
+divChild2.style.borderRadius = "5px";
+console.log(divChild2);
+
+// second child er vitore div er use................
+
+let ulHeading2 = document.getElementById("ulhead2");
+ulHeading2.style.fontSize = "20px";
+ulHeading2.style.color = "white";
+ulHeading2.style.textAlign = "center";
+console.log(ulHeading2);
+
+let underList2 = document.getElementById("ul2");
+underList2.innerHTML =
+  "<li> second list </li> <li> second list </li> <li> second list </li> <li> second list </li> <li> second list </li>";
+underList2.style.fontSize = "18px";
+underList2.style.color = "blue";
+console.log(underList2);
+
+// third child................
+
+let divChild3 = document.getElementsByClassName("child")[2];
+divChild3.style.backgroundColor = "red";
+divChild3.style.height = "200px";
+divChild3.style.width = "300px";
+divChild3.style.border = "2px";
+divChild3.style.borderRadius = "5px";
+console.log(divChild3);
+
+// third child er vitore div er use..............
+
+let ulHeading3 = document.getElementById("ulhead3");
+ulHeading3.style.fontSize = "20px";
+ulHeading3.style.color = "white";
+ulHeading3.style.textAlign = "center";
+console.log(ulHeading3);
+
+let underList3 = document.getElementById("ul3");
+underList3.innerHTML =
+  "<li> second list </li> <li> second list </li> <li> second list </li> <li> second list </li> <li> second list </li>";
+underList3.style.fontSize = "18px";
+underList3.style.color = "yellow";
+console.log(underList3);
+
+// div2 start.....................................................
+
+let div1Parent2 = document.getElementById("parent2");
+div1Parent2.style.display = "Flex";
+div1Parent2.style.justifyContent = "center";
+div1Parent2.style.justifyItems = "center";
+div1Parent2.style.gap = "40px";
+div1Parent2.style.marginTop = "20px";
+console.log(div1Parent2);
+
+// child2-1....................
+
+let divChildOne1 = document.getElementsByClassName("child2")[0];
+divChildOne1.style.height = "200px";
+divChildOne1.style.width = "300px";
+divChildOne1.style.border = "solid";
+divChildOne1.style.border = "2pxRed";
+divChildOne1.style.borderRadius = "5px";
+console.log(divChildOne1);
+
+// child2-1 er vitore innerhtml er use...........
+
+let ulHeadingOne1 = document.getElementById("ulheading1");
+ulHeadingOne1.style.fontSize = "20px";
+ulHeadingOne1.style.color = "Red";
+ulHeadingOne1.style.textAlign = "center";
+console.log(ulHeadingOne1);
+
+let underListOne1 = document.getElementById("li1");
+underListOne1.innerHTML =
+  "<li> second list </li> <li> second list </li> <li> second list </li> <li> second list </li> <li> second list </li>";
+underListOne1.style.fontSize = "18px";
+underListOne1.style.color = "red";
+console.log(underListOne1);
+
+// child2-2....................
+let divChildOne2 = document.getElementsByClassName("child2")[1];
+divChildOne2.style.height = "200px";
+divChildOne2.style.width = "300px";
+divChildOne2.style.border = "solid";
+divChildOne2.style.border = "2pxRed";
+divChildOne2.style.borderRadius = "5px";
+console.log(divChildOne2);
+
+// child2-2 er vitore innerhtml er use...........
+
+let ulHeadingOne2 = document.getElementById("ulheading2");
+ulHeadingOne2.style.fontSize = "20px";
+ulHeadingOne2.style.color = "Blue";
+ulHeadingOne2.style.textAlign = "center";
+console.log(ulHeadingOne2);
+
+let underListOne2 = document.getElementById("li2");
+underListOne2.innerHTML =
+  "<li> second list </li> <li> second list </li> <li> second list </li> <li> second list </li> <li> second list </li>";
+underListOne2.style.fontSize = "18px";
+underListOne2.style.color = "Blue";
+console.log(underListOne2);
+
+// child2-3....................
+
+let divChildOne3 = document.getElementsByClassName("child2")[2];
+divChildOne3.style.height = "200px";
+divChildOne3.style.width = "300px";
+divChildOne3.style.border = "solid";
+divChildOne3.style.border = "2pxRed";
+divChildOne3.style.borderRadius = "5px";
+console.log(divChildOne3);
+
+// child2-2 er vitore innerhtml er use...........
+
+let ulHeadingOne3 = document.getElementById("ulheading3");
+ulHeadingOne3.style.fontSize = "20px";
+ulHeadingOne3.style.color = "brown";
+ulHeadingOne3.style.textAlign = "center";
+console.log(ulHeadingOne3);
+
+let underListOne3 = document.getElementById("li3");
+underListOne3.innerHTML =
+  "<li> second list </li> <li> second list </li> <li> second list </li> <li> second list </li> <li> second list </li>";
+underListOne3.style.fontSize = "18px";
+underListOne3.style.color = "brown";
+console.log(underListOne3);
